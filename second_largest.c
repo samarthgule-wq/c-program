1c60ecca5412584bd7e11695019b1ed50f32e5be
student samarth-guledgudd <samarthgule@gmail.com>
2026-04-03 23:15:49 +0530

    new c programm second large number

---
+++ second_largest.c
@@ -0,0 +1,35 @@
+#include<stdio.h>
+void main()
+{
+    int a,b,c;
+    printf("enter the number for a,b,c:");
+    scanf("%d%d%d",&a,&b,&c);
+    if(a>b){
+        if(a>c){
+            if(b>c){
+                printf("second largest number is:%d",b);
+            }
+            else{
+                printf("second largest number is:%d",c);
+            }
+        }else{
+            printf("second largest number is:%d",a);
+        }    
+    }
+    else{
+        if(b>c){
+            if(a>c){
+                printf("second largest number is:%d",a);
+            }
+            else{
+                printf("second largest number is:%d",c);
+            } 
+        }
+        else{
+            printf("second largest number is:%d",b);
+        }
+    }
+}
+                
+        
+    
