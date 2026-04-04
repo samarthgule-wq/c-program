fdc568389736fd1bbdbd3fade1e34e0cf234efec
student samarth-guledgudd <samarthgule@gmail.com>
2026-04-04 22:48:09 +0530

    c programm for vowel

---
+++ vowel.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+void main()
+{
+    char ch;
+    printf("Enter a cahrecter:");
+    scanf("%c",&ch);
+    switch(ch){
+        case 'a':
+        case 'e':
+        case 'i':       
+        case 'o':
+        case 'u':
+        case 'A':
+        case 'E':
+        case 'I':
+        case 'O':
+        case 'U':
+        printf("It is a vowel\n");
+        break;
+        default:
+        printf("It is a consonant\n");
+    }
+}
