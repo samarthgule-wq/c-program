f7ab9bcccbdf9b99883338ff7e46c2db6e0b5842
student samarth-guledgudd <samarthgule@gmail.com>
2026-04-05 21:56:37 +0530

    c programm for decimal to binary

---
+++ decimal_binary.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int main() {
+    int decimal;
+    printf("Enter a decimal number: ");
+    scanf("%d", &decimal);
+    
+    if (decimal == 0) {
+        printf("Binary: 0\n");
+        return 0;
+    }
+    
+    int binary[32]; // Array to store binary digits
+    int i = 0;
+    
+    while (decimal > 0) {
+        binary[i] = decimal % 2;
+        decimal /= 2;
+        i++;
+    }
+    
+    printf("Binary: ");
+    for (int j = i - 1; j >= 0; j--) {
+        printf("%d", binary[j]);
+    }
+    printf("\n");
+    
+    return 0;
+}
